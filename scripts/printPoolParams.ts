cfa62a5a86bb6fa46fb7956322ac86754fadc45a
vesu-dev <174601598+vesu-dev@users.noreply.github.com>
2025-09-23 14:55:16 +0100

    Fmt

--- scripts/printPoolParams.ts
+++ scripts/printPoolParams.ts
@@ -22,8 +22,7 @@ for (const [index, asset] of config.asset_params.entries()) {
   console.log("Number of sources:         ", oracle_config.number_of_sources.toString());
   console.log("Start time offset:         ", oracle_config.start_time_offset.toString());
   console.log("Time window:               ", oracle_config.time_window.toString());
-  console.log("Aggregation mode:          ", Object.keys(oracle_config.aggregation_mode.variant)[0]
-);
+  console.log("Aggregation mode:          ", Object.keys(oracle_config.aggregation_mode.variant)[0]);
 
   const interest_rate_config = await pool.interest_rate_config(asset.asset);
   console.log("Min target utilization:    ", Number(interest_rate_config.min_target_utilization) / 100_000);
@@ -33,7 +32,7 @@ for (const [index, asset] of config.asset_params.entries()) {
   console.log("Max full utilization rate: ", Number(interest_rate_config.max_full_utilization_rate) / 1e18);
   console.log("Zero utilization rate:     ", interest_rate_config.zero_utilization_rate.toString());
   console.log("Rate half life:            ", Number(interest_rate_config.rate_half_life) / 60 / 60, "hours");
-  console.log("Target rate percent:       ", Number(interest_rate_config.target_rate_percent) / 1e16, "%"); 
+  console.log("Target rate percent:       ", Number(interest_rate_config.target_rate_percent) / 1e16, "%");
 
   const asset_config = await pool.asset_config(asset.asset);
   let symbol = shortString.decodeShortString(await protocol.assets[index].symbol());
@@ -50,8 +49,8 @@ for (const [index, asset] of config.asset_params.entries()) {
   console.log("Fee rate:                  ", Number(asset_config.fee_rate) / 1e16, "%");
 
   console.log("Price:                     ", Number((await pool.price(asset.asset)).value) / 1e18);
-  console.log("Rate accumulator:          ", Number((await pool.rate_accumulator(asset.asset))) / 1e18);
-  console.log("Utilization:               ", Number((await pool.utilization(asset.asset))) / 1e1);
+  console.log("Rate accumulator:          ", Number(await pool.rate_accumulator(asset.asset)) / 1e18);
+  console.log("Utilization:               ", Number(await pool.utilization(asset.asset)) / 1e1);
 
   console.log("");
 }
@@ -67,7 +66,7 @@ for (const [, asset] of config.pair_params.entries()) {
   console.log(`${collateral_asset_symbol} / ${debt_asset_symbol}`);
   console.log("Max LTV:                   ", Number(pair_config.max_ltv) / 1e18);
   console.log("Liquidation factor:        ", Number(pair_config.liquidation_factor) / 1e18);
-  console.log("Debt cap:                  ", Number(pair_config.debt_cap) / 10**Number(decimals), debt_asset_symbol);
+  console.log("Debt cap:                  ", Number(pair_config.debt_cap) / 10 ** Number(decimals), debt_asset_symbol);
 
   console.log("");
 }
